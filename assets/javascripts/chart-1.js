950973938e38395b5542fd3014dce84400ffa30e
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-09-12 12:04:22 -0400

    Chart 1: Add toggle functionality

--- assets/javascripts/chart-1.js
+++ assets/javascripts/chart-1.js
@@ -1,18 +1,52 @@
 d3.csv("/assets/data/ele_assis_monthly_cnt.csv").then( data => {
-    //X AXIS: yr_qtr (ex: trip_q4_2018)
-    //Y AXIS: total_count (ex: 4093)
     var margin = {top: 50, right: 50, bottom: 50, left: 50}
     , width = 800 - margin.left - margin.right
-    , height = 600 - margin.top - margin.bottom;
+    , height = 650 - margin.top - margin.bottom;
+
+    const toggleQuarters = document.querySelector(".quarterly")
+    const toggleMonths = document.querySelector(".monthly")
+
+    toggleMonths.addEventListener("click", function() {
+        d3.selectAll(".ridetype-quarterly")
+        .attr("visibility", "hidden")
+
+        d3.select(".xaxis-quarterly")
+        .attr("visibility", "hidden")
+
+        d3.selectAll(".ridetype-monthly")
+        .attr("visibility", "visible")
+
+        d3.select(".xaxis-monthly")
+        .attr("visibility", "visible")
+    })
+
+    toggleQuarters.addEventListener("click", function() {
+        d3.selectAll(".ridetype-monthly")
+        .attr("visibility", "hidden")
+
+        d3.select(".xaxis-monthly")
+        .attr("visibility", "hidden")
+
+        d3.selectAll(".ridetype-quarterly")
+        .attr("visibility", "visible")
+
+        d3.select(".xaxis-quarterly")
+        .attr("visibility", "visible")
+    })
 
     var dataset = d3.stack()
         .keys(["ele_prcntg", "mechanical_prcntg"])
         const series = dataset(data)
 
-    var x = d3.scaleBand()
-            .range([0, width])
-            .padding(0.1)
-            .domain(data.map( d => { return d.yr_qtr}))
+    var xQuarterly = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1)
+        .domain(data.map( d => { return d.yr_qtr}))
+    
+    var xMonthly = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1)
+        .domain(data.map( d => { return d._month}))
 
     var y = d3.scaleLinear()
         .range([height, 0])
@@ -33,122 +67,51 @@ d3.csv("/assets/data/ele_assis_monthly_cnt.csv").then( data => {
 	.attr("y", 6)
 	.attr("dy", "0.71em")
 	.attr("text-anchor", "end")
-    .text("% of Rides");
+    .text("% of Rides")
 
     var colors = ["#d25c4d","#d9d574"];
-    g.append("g")
-        .attr("class", "xaxis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
 
-        var groups = svg.selectAll("g.ridetype")
+    g.append("g")
+        .attr("class", "xaxis-quarterly")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xQuarterly))
+    
+    g.append("g")
+        .attr("class", "xaxis-monthly")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xMonthly))
+        .attr("visibility","hidden")
+
+    const quarterlyLayers = svg.selectAll("g.ridetype-quarterly")
         .data(series)
         .enter().append("g")
-        .attr("class", "ridetype")
+        .attr("class", "ridetype-quarterly")
         .style("fill", function(d, i) { return colors[i]; })
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-    groups.selectAll("rect")
+    quarterlyLayers.selectAll("rect")
         .data(function(d) { return d; })
         .enter().append("rect")
-        .attr("x", function(d) { return x(d.data.yr_qtr) })
+        .attr("x", function(d) { return xQuarterly(d.data.yr_qtr) })
         .attr("y", d => { return y(d[1]) })
         .attr("height", d => Math.abs(y(d[1]) - y(d[0])))
-        .attr("width", x.bandwidth())
+        .attr("width", xQuarterly.bandwidth())
 
-    
+    const monthlyLayers = svg.selectAll("g.ridetype-monthly")
+        .data(series)
+        .enter().append("g")
+        .attr("class", "ridetype-monthly")
+        .style("fill", function(d, i) { return colors[i]; })
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("visibility","hidden")
 
-    const toggleQuarters = document.querySelector("input[value='quarterly']")
-    const toggleMonths = document.querySelector("input[value='monthly']")
-
-   
-
-    toggleQuarters.addEventListener("click", function(e) {
-        var dataset = d3.stack()
-        .keys(["ele_prcntg", "mechanical_prcntg"])
-        const series = dataset(data)
-
-        x.domain(data.map( d => { return d.yr_qtr}))
-
-        d3.select(".xaxis")
-            .remove()
-        
-            d3.selectAll("g.ridetype")
-            .selectAll("rect")
-            .attr("class","edit")
-
-        g.append("g")
-            .attr("class", "xaxis")
-	        .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x))
-
-        var groups = svg.selectAll("g.ridetype")
-            .data(series)
-            .enter().append("g")
-            .attr("class", "ridetype")
-            .style("fill", function(d, i) { return colors[i]; })
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        groups.selectAll("rect")
-            .data(function(d) { return d; })
-            .enter().append("rect")
-            .attr("x", function(d) { return x(d.data.yr_qtr) })
-            .attr("y", d => { return y(d[1]) })
-            .attr("height", d => Math.abs(y(d[1]) - y(d[0])))
-            .attr("width", x.bandwidth())
-    })
-
-    toggleMonths.addEventListener("click", function(e) {
-        var dataset = d3.stack()
-        .keys(["ele_prcntg", "mechanical_prcntg"])
-
-        const series = dataset(data)
-
-        x.domain(data.map( d => { return d._month}))
-
-        d3.select(".xaxis")
-            .remove()
-        
-        d3.selectAll("g.ridetype")
-        .selectAll("rect").exit()
-            .remove()
-
-
-        g.append("g")
-            .attr("class", "xaxis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x))
-
-        var groups = svg.selectAll("g.ridetype")
-            .data(series)
-            .enter().append("g")
-            .attr("class", "ridetype")
-            .style("fill", function(d, i) { return colors[i]; })
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        groups.selectAll("rect")
-            .data(function(d) { return d; })
-            .enter().append("rect")
-            .attr("x", function(d) { return x(d.data._month) })
-            .attr("y", d => { return y(d[1]) })
-            .attr("height", d => Math.abs(y(d[1]) - y(d[0])))
-            .attr("width", x.bandwidth())
-    })
-
-    // var groups = svg.selectAll("g.ridetype")
-    // .data(series)
-    // .enter().append("g")
-    // .attr("class", "ridetype")
-    // .style("fill", function(d, i) { return colors[i]; })
-    // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // groups.selectAll("rect")
-    // .data(function(d) { return d; })
-    // .enter().append("rect")
-    // .attr("x", function(d) { return x(d.data.yr_qtr) })
-    // .attr("y", d => { return y(d[1]) })
-	// .attr("height", d => Math.abs(y(d[1]) - y(d[0])))
-    // .attr("width", x.bandwidth())
+    monthlyLayers.selectAll("rect")
+        .data(function(d) { return d; })
+        .enter().append("rect")
+        .attr("x", function(d) { return xMonthly(d.data._month) })
+        .attr("y", d => { return y(d[1]) })
+        .attr("height", d => Math.abs(y(d[1]) - y(d[0])))
+        .attr("width", xMonthly.bandwidth())
 
     createLegend(colors)
 })
@@ -156,7 +119,7 @@ d3.csv("/assets/data/ele_assis_monthly_cnt.csv").then( data => {
 function createLegend(colors){
     const legend = d3.select(".chart-1").append("svg")
     .attr("class","legend")
-    .attr("transform", `translate(50,600)`)
+    .attr("transform", `translate(50, 0)`)
     .attr("width", 200)
     .attr("height", 50)
 
