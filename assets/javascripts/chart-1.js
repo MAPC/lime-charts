9759ff27ad88980561f1c26f52decbe62cda0eb5
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-09-13 10:42:57 -0400

    Chart 1: Fix axes, legend, toggle, and font sizes

--- assets/javascripts/chart-1.js
+++ assets/javascripts/chart-1.js
@@ -1,50 +1,19 @@
 d3.csv("/assets/data/ele_assis_monthly_cnt.csv").then( data => {
     var margin = {top: 50, right: 75, bottom: 50, left: 75}
-    , width = 700 - margin.left - margin.right
+    , width = 800 - margin.left - margin.right
     , height = 650 - margin.top - margin.bottom;
-
-    const toggleQuarters = document.querySelector(".quarterly")
-    const toggleMonths = document.querySelector(".monthly")
-
-    toggleMonths.addEventListener("click", function() {
-        d3.selectAll(".ridetype-quarterly")
-        .attr("visibility", "hidden")
-
-        d3.select(".xaxis-quarterly")
-        .attr("visibility", "hidden")
-
-        d3.selectAll(".ridetype-monthly")
-        .attr("visibility", "visible")
-
-        d3.select(".xaxis-monthly")
-        .attr("visibility", "visible")
-    })
-
-    toggleQuarters.addEventListener("click", function() {
-        d3.selectAll(".ridetype-monthly")
-        .attr("visibility", "hidden")
-
-        d3.select(".xaxis-monthly")
-        .attr("visibility", "hidden")
-
-        d3.selectAll(".ridetype-quarterly")
-        .attr("visibility", "visible")
-
-        d3.select(".xaxis-quarterly")
-        .attr("visibility", "visible")
-    })
 
     var dataset = d3.stack()
         .keys(["ele_prcntg", "mechanical_prcntg"])
         const series = dataset(data)
 
     var xQuarterly = d3.scaleBand()
-        .range([0, width])
+        .range([0, width-margin.right])
         .padding(0.1)
         .domain(data.map( d => { return d.yr_qtr}))
     
     var xMonthly = d3.scaleBand()
-        .range([0, width])
+        .range([0, width-margin.right])
         .padding(0.1)
         .domain(data.map( d => { return d._month}))
 
@@ -55,6 +24,11 @@ d3.csv("/assets/data/ele_assis_monthly_cnt.csv").then( data => {
     var svg = d3.select(".chart-1")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
+    
+    svg.append("text")
+        .attr("class", "graph__title")
+        .text("E-Bike vs. Manual Bike Rides, Quarterly or Monthly")
+        .attr("transform", `translate(125, 15)`)
 
     var graph = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
@@ -62,19 +36,87 @@ d3.csv("/assets/data/ele_assis_monthly_cnt.csv").then( data => {
         .attr("height", height-margin.top)
     
     graph.append("g")
-	    .call(d3.axisLeft(y))
+        .call(d3.axisLeft(y)
+                .tickFormat(d3.format("~p")))
     
     svg.append("text")
 	.attr("fill", "#000")
 	.attr("transform", "translate(25, 175) rotate(-90)")
 	.attr("text-anchor", "end")
     .text("Percentage (%) of Rides")
+    .attr("class", "axis-label")
 
     svg.append("text")
 	.attr("fill", "#000")
-	.attr("transform", `translate(325, ${height + margin.top +25})`)
+	.attr("transform", `translate(375, ${height + margin.top + margin.bottom})`)
 	.attr("text-anchor", "end")
     .text("Time")
+    .attr("class", "axis-label")
+
+    const toggle = svg.append("svg")
+        .attr("class", "toggle__section")
+        .attr("x", `${width + 25}`)
+        .attr("y", 140)
+        .attr("width", 125)
+        .attr("height", 100)
+    
+    toggle.append("text")
+        .text("Timeframe")
+        .attr("transform", "translate(5,25)")
+        .attr("class", "toggle__title")
+    
+    toggle.append("text")
+        .text("- Quarterly")
+        .attr("transform", "translate(5,50)")
+        .attr("class", "toggle__item toggle__item-active quarterly")
+    
+    toggle.append("text")
+        .text("- Monthly")
+        .attr("transform", "translate(5,70)")
+        .attr("class", "toggle__item monthly")
+    
+    const toggleQuarters = document.querySelector(".quarterly")
+    const toggleMonths = document.querySelector(".monthly")
+    
+        toggleMonths.addEventListener("click", function() {
+            d3.selectAll(".ridetype-quarterly")
+            .attr("visibility", "hidden")
+    
+            d3.select(".xaxis-quarterly")
+            .attr("visibility", "hidden")
+    
+            d3.selectAll(".ridetype-monthly")
+            .attr("visibility", "visible")
+    
+            d3.select(".xaxis-monthly")
+            .attr("visibility", "visible")
+
+            d3.select('.monthly')
+            .classed("toggle__item-active", true)
+
+            d3.select('.quarterly')
+            .classed("toggle__item-active", false)
+        })
+    
+        toggleQuarters.addEventListener("click", function() {
+            d3.selectAll(".ridetype-monthly")
+            .attr("visibility", "hidden")
+    
+            d3.select(".xaxis-monthly")
+            .attr("visibility", "hidden")
+    
+            d3.selectAll(".ridetype-quarterly")
+            .attr("visibility", "visible")
+    
+            d3.select(".xaxis-quarterly")
+            .attr("visibility", "visible")
+
+            d3.select('.monthly')
+            .classed("toggle__item-active", false)
+
+            d3.select('.quarterly')
+            .classed("toggle__item-active", true)
+        })
 
     var colors = ["#1b5eb8", "#5eb81b"]
 
@@ -128,35 +170,45 @@ d3.csv("/assets/data/ele_assis_monthly_cnt.csv").then( data => {
         .attr("height", d => Math.abs(y(d[1]) - y(d[0])))
         .attr("width", xMonthly.bandwidth())
 
-    createLegend(colors)
+    createLegend(colors,width)
 })
 
-function createLegend(colors){
+function createLegend(colors, width){
     const legend = d3.select(".chart-1").append("svg")
     .attr("class","legend")
-    .attr("transform", `translate(50, 0)`)
-    .attr("width", 200)
-    .attr("height", 50)
+    .attr("left", "625")
+    .attr("x", `${width + 25}`)
+    .attr("y", 40)
+    .attr("display", "inline-block")
+    .attr("width", 125)
+    .attr("height", 100)
 
+    legend.append("text")
+    .text("Legend")
+    .attr("class", "legend__title")
+    .attr("transform", "translate(5,25)")
+    
     legend.append("circle")
     .attr("fill", colors[0])
     .attr("r",5)
-    .attr("transform", "translate(5,10)")
+    .attr("transform", "translate(10,50)")
 
     legend.append("text")
-    .attr("transform", "translate(15,15)")
+    .attr("transform", "translate(20,55)")
     .text("E-Bikes")
+    .attr("class", "legend__item")
 
     ///
 
     legend.append("circle")
     .attr("fill", colors[1])
     .attr("r",5)
-    .attr("transform", "translate(5,30)")
+    .attr("transform", "translate(10,70)")
 
     legend.append("text")
-    .attr("transform", "translate(15,35)")
+    .attr("transform", "translate(20,75)")
     .text("Mechanical Bikes")
+    .attr("class", "legend__item")
 
     
 }
