7572977391088c2e952434ac66534ab42075c2bc
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-09-14 19:57:45 -0400

    Chart 4: fix scaling issue

--- assets/javascripts/chart-4.js
+++ assets/javascripts/chart-4.js
@@ -1,13 +1,33 @@
+d3.csv("/assets/data/ele_assis_monthly_cnt_copy.csv").then(data => {
 
-d3.csv("/assets/data/ele_assis_monthly_cnt_copy.csv").then( data => {
     var margin = {top: 50, right: 75, bottom: 50, left: 75}
     , width = 800 - margin.left - margin.right
     , height = 650 - margin.top - margin.bottom;
 
+
+    const quarterlyData = data.reduce(function(quarters,month) {
+        let quarter = quarters.find(quarter => { return quarter.yr_qtr === month.yr_qtr })
+        if (!quarter) {
+            quarter = { yr_qtr: month.yr_qtr,
+                        total_count: 0,
+                        mechanical: 0,
+                        ele_assis: 0 }
+            quarters.push(quarter)
+        }
+
+        quarter.total_count += parseInt(month.total_count)
+        quarter.mechanical += parseInt(month.mechanical)
+        quarter.ele_assis += parseInt(month.ele_assis)
+        return quarters
+    }, [])
+
+    console.log(quarterlyData)
+
     var dataset = d3.stack()
-        .keys(["total_count"])
-        const series = dataset(data)
-    console.log(series)
+    .keys(["ele_assis", "mechanical"])
+    const series = dataset(data)
+
+    const quarterlySeries = dataset(quarterlyData)
 
     var xQuarterly = d3.scaleBand()
         .range([0, width-margin.right])
@@ -21,7 +41,7 @@ d3.csv("/assets/data/ele_assis_monthly_cnt_copy.csv").then( data => {
 
     var y = d3.scaleLinear()
         .range([height-margin.top, 0])
-        .domain([0, 45000])
+        .domain([0, 120000])
 
     var svg = d3.select(".chart-4")
         .attr("width", width + margin.left + margin.right)
@@ -144,7 +164,7 @@ d3.csv("/assets/data/ele_assis_monthly_cnt_copy.csv").then( data => {
         .attr("transform", "rotate(-65)");
 
     const quarterlyLayers = graph.selectAll("g.ridetype-quarterly")
-        .data(series)
+        .data(quarterlySeries)
         .enter().append("g")
         .attr("class", "ridetype-quarterly")
         .style("fill", function(d, i) { return colors[i]; })
